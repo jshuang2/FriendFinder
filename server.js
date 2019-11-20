702d75359ff9cb9d822c0236a4d75b941fa3bfcf
Jeff Huang <jeffshuang2@gmail.com>
2019-11-20 12:45:12 -0800

    Added Bootstrap links and created initial HTML for the homepage.

--- server.js
+++ server.js
@@ -5,9 +5,15 @@ var bodyParser = require("body-parser");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
 app.get("/", function (req, res) {
     res.send("Hello World!");
-})
+});
+
+
 
 
 
