2f36ae5d19ba8728254eb3eb72229eedf11a5959
Jeff Huang <jeffshuang2@gmail.com>
2019-11-20 13:05:20 -0800

    Created an array to hold data for friends and set it up for export.

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+var friendsArray = [
+    {
+        name: "Michael",
+        photo: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+        scores: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            1,
+            2,
+            3,
+            4,
+            5
+        ]
+    }
+]
+
+
+
+
+
+module.exports = friendsArray;
