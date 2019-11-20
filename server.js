97fbf46cc9758a9a2a53bebc2c448d2c3d953494
Jeff Huang <jeffshuang2@gmail.com>
2019-11-20 12:00:35 -0800

    Installed packages. Added node_modules to .gitignore. Successfully created node server to listen on localhost.

--- server.js
+++ server.js
@@ -1,2 +1,21 @@
 var express = require("express");
 var path = require("path");
+var bodyParser = require("body-parser");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.get("/", function (req, res) {
+    res.send("Hello World!");
+})
+
+
+
+
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+})
+
