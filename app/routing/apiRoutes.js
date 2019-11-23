2a6a724b1468cbcb6bbf3e13c84f0e83dbd3aefa
Jeff Huang <jeffshuang2@gmail.com>
2019-11-22 19:57:31 -0800

    App now properly captures user input and stores that data as JSON.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,4 +4,13 @@ module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
         res.json(surveyData);
     });
+
+    app.post("/api/friends", function (req, res) {
+        var newSurveyResult = req.body;
+        
+        surveyData.push(newSurveyResult);
+
+        res.json(newSurveyResult);
+
+    })
 };
